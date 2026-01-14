9a570a542d0e41b0cec8e6bf1a8aee5a763aaa55
adityachaudhari26 <aditya.chaudhari950@gmail.com>
2026-01-14 14:15:09 +0530

    Add helloworld and arthimatic operator

---
+++ Arthimatic_Operator.java
@@ -0,0 +1,24 @@
+public class Arthimatic_Operator {
+
+    public static void main(String[] args) {
+        System.out.println("================Arthimatic Operator========================");
+
+        int add = 12+14;
+        System.out.println("Addition is :" + add);
+        
+        int sub = 36-10;
+        System.out.println("Substraction is :" + sub);
+
+        int mul = 13*2;
+        System.out.println("Multipication is :" + mul);
+
+        int div = 30/2;
+        System.out.println("Division is :" + div);
+
+        int mod = 40%4;
+        System.out.println("Modulus is :" + mod);
+        
+
+
+    }
+}
